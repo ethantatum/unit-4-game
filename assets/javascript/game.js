257ce666fe64ed34f38fbb00155628c3cee149f8
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-11-21 16:58:44 -0700

    updated html, added crystal images, started JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    // VARIABLES
+    // ==================================================================================
+    let wins = 0;
+    let losses = 0;
+
+
+
+    // FUNCTIONS
+    // ==================================================================================
+    
+
+    $(`#wins`).html(wins);
+    $(`#losses`).html(losses);
+
+
+
+
+    $(`#ruby-click`).on(`click`, function() {
+        console.log(`You clicked the ruby!`);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
