777c40a99dee4df4c8d6ae2a422965cf475ea1c8
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-11-22 07:46:03 -0700

    added functionality for clicking crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,64 @@
 $(document).ready(function() {
     // VARIABLES
     // ==================================================================================
+    const gemValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    
     let wins = 0;
     let losses = 0;
-
+    let counter = 0;
 
 
     // FUNCTIONS
     // ==================================================================================
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
     
 
+    function newGame() {
+        let currentRandom = randomNumber(19, 120);
+        $(`#random-number`).html(currentRandom);
+        
+        let rubyValue = gemValues[Math.floor(Math.random() * gemValues.length)];
+        gemValues.splice(gemValues.indexOf(rubyValue), 1);
+        let diamondValue = gemValues[Math.floor(Math.random() * gemValues.length)];
+        gemValues.splice(gemValues.indexOf(diamondValue), 1);
+        let sapphireValue = gemValues[Math.floor(Math.random() * gemValues.length)];
+        gemValues.splice(gemValues.indexOf(sapphireValue), 1);
+        let emeraldValue = gemValues[Math.floor(Math.random() * gemValues.length)];
+
+        console.log(rubyValue);
+        console.log(diamondValue);
+        console.log(sapphireValue);
+        console.log(emeraldValue);
+
+        $(`#ruby-click`).on(`click`, function() {
+            counter += rubyValue;
+            $(`#current-total`).html(counter);
+
+        })
+        $(`#diamond-click`).on(`click`, function() {
+            counter += diamondValue;
+            $(`#current-total`).html(counter);
+        })
+        $(`#sapphire-click`).on(`click`, function() {
+            counter += sapphireValue;
+            $(`#current-total`).html(counter);
+        })
+        $(`#emerald-click`).on(`click`, function() {
+            counter += emeraldValue;
+            $(`#current-total`).html(counter);
+        })
+
+
+    }
+
+    newGame();
     $(`#wins`).html(wins);
     $(`#losses`).html(losses);
 
 
 
-
-    $(`#ruby-click`).on(`click`, function() {
-        console.log(`You clicked the ruby!`);
-    })
 
 
 
